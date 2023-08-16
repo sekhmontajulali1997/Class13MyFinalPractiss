4bd1b30ca48e63d8fe07ce03780fec6f73caf88d
skMontajulAli <sekhmontajulali1997@gmail.com>
2023-08-16 19:23:30 +0530

    delate button done

--- app/functions.js
+++ app/functions.js
@@ -22,6 +22,48 @@ const getDataLS = (key) => {
     }
 
 };
+// post time functions
 
+function getFacebookPostTime(postDate) {
+    const currentDate = new Date();
+    const postDateTime = new Date(postDate);
+    const timeDiffInSeconds = Math.floor((currentDate - postDateTime) / 1000);
+  
+    if (timeDiffInSeconds < 60) {
+      return `${timeDiffInSeconds} seconds ago`;
+    } else if (timeDiffInSeconds < 3600) {
+      const minutes = Math.floor(timeDiffInSeconds / 60);
+      return `${minutes} minute${minutes === 1 ? '' : 's'} ago`;
+    } else if (timeDiffInSeconds < 86400) {
+      const hours = Math.floor(timeDiffInSeconds / 3600);
+      return `${hours} hour${hours === 1 ? '' : 's'} ago`;
+    } else if (timeDiffInSeconds < 604800) {
+      const days = Math.floor(timeDiffInSeconds / 86400);
+      return `${days} day${days === 1 ? '' : 's'} ago`;
+    } else {
+      // If more than a week, display the full date
+      const options = { year: 'numeric', month: 'long', day: 'numeric' };
+      return postDateTime.toLocaleDateString(undefined, options);
+    }
+  }
+  
+  // Example usage:
+  const postDate = "2023-08-02T15:30:00"; // Replace this with your post date
+  const fbPostTime = getFacebookPostTime(postDate);
+ 
+  
 
+// reandom id genarator
 
+  function generateRandomID() {
+    const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const idLength = 26;
+    let randomID = '';
+  
+    for (let i = 0; i < idLength; i++) {
+      const randomIndex = Math.floor(Math.random() * chars.length);
+      randomID += chars.charAt(randomIndex);
+    }
+  
+    return randomID;
+  };
--- app/main.js
+++ app/main.js
@@ -1,6 +1,71 @@
 const students_add_Form = document.querySelector('#students_add_Form');
 const for_auto_close = document.querySelector('.for_auto_close');
-const validationAlert = document.querySelector('.validationAlert');
+const validationAlert = document.querySelector('.validationAlert');   
+const studentsListt = document.querySelector('.studentsListt');   
+
+
+
+// show students list
+
+const showStudentsList = () =>{
+
+    const alldata = getDataLS('students_Data');
+
+    let studentsList = '';
+
+    if (alldata.length > 0) {
+
+        alldata.map((item, index) => {
+
+
+            
+            studentsList +=`
+            
+            <tr class="text-center align-middle">
+            <th scope="row">${index+1}</th>
+            <td><img style="width: 60px; height: 60px; border-radius: 50%; object-fit: cover;"  src="${item.students_photo}" alt=""></td>
+            <td>${item.students_name}</td>
+            <td>${item.students_roll}</td>
+            <td>${item.students_reg_no}</td>
+            <td>${getFacebookPostTime(item.create_at)}</td>
+            <td><Button class=" add_marksbtn"> Add Marks</Button></td>
+            <td>
+            <i class="fa-regular fa-eye forPreview"></i>
+            <i class="fa-regular fa-pen-to-square forEdit"></i>
+            <i  class="fa-solid fa-trash forTrash" onclick = " fotDelete('${item.id}')"></i>
+           
+            </td>
+          </tr>
+            
+            `
+
+        })
+
+    }else{
+
+        studentsList = ` <td colspan = "8"> <h4 style= "text-align: center; color:red;"> Please Create Students List  </h4>  </td>`
+
+    }
+    studentsListt.innerHTML = studentsList;
+}
+showStudentsList();
+
+
+// delete students list
+
+const fotDelete = (id) =>{
+    const alldata = getDataLS('students_Data');
+    const updateDta = alldata.filter((data) => data.id !== id);
+    setDataLS('students_Data',updateDta );
+    showStudentsList();
+
+}
+
+
+
+
+
+
 
 
 students_add_Form.onsubmit = (e) =>{
@@ -28,10 +93,12 @@ students_add_Form.onsubmit = (e) =>{
             students_roll: allData.students_roll,
             students_reg_no: allData.students_reg_no,
             create_at: Date.now(),
+            id: generateRandomID(26),
             result: null,
         }
     );
     setDataLS('students_Data',prev );
+    showStudentsList();
         for_auto_close.click();
      };
   
